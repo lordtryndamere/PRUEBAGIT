b3bb1de13a575f365d9ab93758779a61b2904b0c
Kevin.JS <iammaindiana@gmail.com>
2019-09-22 11:23:47 -0500

    Ultima version

---
+++ finalprimos.js
@@ -0,0 +1,11 @@
+var n1 = prompt(parseInt("Por  Favor ingresa un numero"));
+var n2 =prompt(parseInt("Por  Favor ingresa un segundo  numero"));
+
+if (n1>n2) {
+    alert(n1, "Es mayor");
+    alert(n2, "Es menor")
+}
+else{
+    alert(n2,"es mayor");
+    alert(n1,"es mmenor");
+}
